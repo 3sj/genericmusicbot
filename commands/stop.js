747a5df4cb22d9a5effd88d0f09fc6037b9cf6a4
WillDyer <37468008+WillDyer@users.noreply.github.com>
2021-09-21 21:28:24 +0100

    Added emoji reaction

--- commands/stop.js
+++ commands/stop.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     if(queue) {
         bot.distube.stop(message)
 
-        message.channel.send(':stop_button: **Song stoped - I have disconnected, goodbye!**')
+        message.react('👍')
     } else if (!queue) {
         return
     };
